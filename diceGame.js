c860dff3690dd9b1ba4984b8dc3f7aa5cef071df
unknown <mdpayne26@yahoo.com>
2019-01-14 16:26:00 -0600

    i just finnished my first section of the second dice roll, im going to add more rolls.

--- diceGame.js
+++ diceGame.js
@@ -1,7 +1,26 @@
-function zombie(diceType) {
-let diceRoll = Math.floor(Math.random() * Math.floor(diceType));
-    if (diceRoll == 0){
+function zombie(citDay) {
+let diceRoll = Math.floor(Math.random() * Math.floor(citDay));
+    if(diceRoll == 0){
         console.log("you are in the city during the day");
+            function zombie1(citDaySrch){
+            let diceRoll = Math.floor(Math.random() * Math.floor(citDaySrch));
+                if(diceRoll == 0){
+                    console.log("you searched a few wrecked cars nearby.");
+                }
+                else if(diceRoll == 1){
+                    console.log("you searched some deserted office buildings down the street");
+                }
+                else if(diceRoll == 2){
+                    console.log("you searched a burnt up nearby gas station");
+                }
+                else if(diceRoll == 3){
+                    console.log("you searched a few nearby store fronts");
+                }
+                else if(diceRoll == 4){
+                    console.log("you found an abandoned building but didnt go inside");
+                }
+            }
+                zombie1(5);
     }
     else if(diceRoll == 1){
         console.log("you are in the city during the night");
@@ -15,3 +34,6 @@ let diceRoll = Math.floor(Math.random() * Math.floor(diceType));
     
 }
 zombie(4);
+
+
+
